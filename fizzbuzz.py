3105ad69b643628e873cd20ec3e741ba3d7f3d85
“yoon308” <yoon970308@gmail.com>
2022-08-18 11:36:16 +0900

    feat: Print fizzbuzz
    condition: i is times of 15 print 'fizzbuzz'
    adjust numerage 1~16 to 1~20

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
-for i in range(1, 16+1):
-    if i%3==0:
+for j in range(1, 20+1):
+    if j%15==0:
+        print('fizzbuzz')
+    elif j%3==0:
         print('fizz')
     else:
-        print(i)
+        print(j)
